00fb85c09e2e31c3f934a16a94b92e18e690bfd3
Code <Code@batc.local>
2018-08-09 10:56:09 -0600

    Initial commit.

---
+++ src/components/ad.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class Ad extends Component {
+  render() {
+    return (
+      <div>
+        <h1>we post video every day but saturday, sunday, holidays in utah and in the united states, and winter and summer olympic</h1>
+            <p>we will do trollface, lego star wars, hitoyou movies, asdfmovie in trains, jokes, family guys, funny of gaming, and minecraft</p>
+            <p>email some  idea at boatrightnick7@gmail.com or on discord( email me if want be on discord )</p>
+        <h2>the top ten youtubers</h2>
+            <ol>
+                <li>markiplier</li>
+                <li>jemma marbles</li>
+                <li>yuya</li>
+                <li>nigahiga</li>
+                <li>evan fong</li>
+                <li>fernan flu</li>
+                <li>smoosh</li>
+                <li>ruben douglas</li>
+                <li>hola soy</li>
+                <li>pewdiepie</li>
+            </ol>
+        <h3>is there a website problem? email the problem at boatrightnick7@gmail.com</h3>
+            <p>dont spam at all.</p>
+        <h4>usa and utah holidays</h4>
+            <ol>
+                <li>new year's day</li>
+                <li>martin luther king, jr. day</li>
+                <li>presidents' day</li>
+                <li>easter monday</li>
+                <li>memoarial day</li>
+                <li>independence day</li>
+                <li>pioneer day</li>
+                <li>labor day</li>
+                <li>colunbus day</li>
+                <li>election day</li>
+                <li>veterans days</li>
+                <li>thanksgiving</li>
+                <li>christmas eve</li>
+                <li>christmas day</li>
+                <li>new year's eve</li>
+            </ol>
+        <h4>little about me.</h4>
+            <p>i like dantdm</p>
+            <p> i like to play minecraft </p>
+            <p>i am a star wars fan</p>
+        <h5>i am your bomb </h5>
+      </div>
+    );
+  } 
+}
+
+export default Ad;
---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Ad from './ad';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is hitoyou</h1>
+        <Ad />
+      </div>
+    );
+  }
+}
+
+export default App;
